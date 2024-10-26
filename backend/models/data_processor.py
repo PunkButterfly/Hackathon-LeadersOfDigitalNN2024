407cd691c3a471b29adc8ae661a94625530485b8
Alexandr Shkolin <a.shkolin@crpt.ru>
2024-10-26 18:36:20 +0300

    Edit processor_v1 by yaml

--- backend/models/data_processor.py
+++ backend/models/data_processor.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
 import random
+import yaml
 
 
 class DataProcessorV0:
+    """
+    Базовая заглушка
+    """
     def process(self, transactions: pd.DataFrame, clients: pd.DataFrame):
         clients['gndr'] = clients['gndr'].replace({'ж': 0, 'м': 1})
         self.processed_data = clients[['accnt_id', 'brth_yr', 'prsnt_age', 'gndr', 'erly_pnsn_flg']].astype(
@@ -18,7 +22,19 @@ class DataProcessorV0:
 
 
 class DataProcessorV1:
+    """
+    Исходный датасет, только клиенты
+    """
+    def __init__(self):
+        with open('./backend/models/configs/processor_v1.yaml', 'r') as file:
+            self.config = yaml.safe_load(file)
+
     def process(self, transactions: pd.DataFrame, clients: pd.DataFrame):
+        clients_columns_typing = self.config['data']['columns']['clients']
+        clients_columns = list(self.config['data']['columns']['clients'].keys())
+
+        clients = clients[clients_columns]
+
         clients['gndr'] = clients['gndr'].map({'ж': 0, 'м': 1})
         clients['accnt_bgn_date'] = clients['accnt_bgn_date'].astype(np.int64) // 1e9
         clients['accnt_status'] = clients['accnt_status'].map({'Накопительный период': 0, 'Выплатной период': 1})
@@ -34,58 +50,8 @@ class DataProcessorV1:
         clients['assgn_npo'] = clients['assgn_npo'].map({'нет': 0, 'да': 1})
         clients['assgn_ops'] = clients['assgn_ops'].map({'нет': 0, 'да': 1})
 
-        clients_columns = [
-            'slctn_nmbr',
-            # 'clnt_id',
-            'accnt_id',
-            'gndr',
-            'brth_yr',
-            'prsnt_age',
-            'accnt_bgn_date',
-            # 'cprtn_prd_d',
-            'accnt_status',
-            'pnsn_age',
-            # 'prvs_npf',
-            # 'brth_plc',
-            # 'addrss_type',
-            # 'rgn',
-            # 'dstrct',
-            # 'city',
-            # 'sttlmnt',
-            # 'pstl_code',
-            'okato',
-            'phn',
-            'email',
-            'lk',
-            'assgn_npo',
-            'assgn_ops',
-
-            'erly_pnsn_flg'
-        ]
-
-        preprocessed_data = clients[clients_columns]
-
-        self.processed_data = preprocessed_data.astype(
-            {
-                'slctn_nmbr': np.int64,
-                # 'clnt_id': 'str',
-                'accnt_id': 'str',
-                'gndr': np.int64,
-                'brth_yr': np.int64,
-                'prsnt_age': np.int64,
-                'accnt_bgn_date': np.int64,
-                # 'cprtn_prd_d': np.int64,
-                'accnt_status': np.int64,
-                'pnsn_age': np.int64,
-                'okato': np.int64,
-                'phn': np.int64,
-                'email': np.int64,
-                'lk': np.int64,
-                'assgn_npo': np.int64,
-                'assgn_ops': np.int64,
-
-                'erly_pnsn_flg': np.int64,
-            }
+        self.processed_data = clients.astype(
+            clients_columns_typing
         )
 
         return self.processed_data
