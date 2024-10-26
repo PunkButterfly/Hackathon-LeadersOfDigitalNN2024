49666880f6f8103197261afac42d9d5c9561ea66
RomanIsaev <mr.romka.01@mail.ru>
2024-10-27 00:13:45 +0300

    upd V2

--- backend/models/data_processor.py
+++ backend/models/data_processor.py
@@ -93,49 +93,47 @@ class DataProcessorV2:
         clients['lk'] = clients['lk'].map({'нет': 0, 'да': 1})
         clients['assgn_npo'] = clients['assgn_npo'].map({'нет': 0, 'да': 1})
         clients['assgn_ops'] = clients['assgn_ops'].map({'нет': 0, 'да': 1})
-
-        clients['accnt_bgn_date'] = pd.to_datetime(clients['accnt_bgn_date'])
-        # Возраст на момент подписания договора
-        clients['age_agreement_start'] = clients['accnt_bgn_date'].dt.year - clients['brth_yr']
-
         # Флаг наличия пред организации
         clients['prvs_npf_flg'] = clients['prvs_npf'].case_when(([(~clients['prvs_npf'].isnull(), 1),
                                                         (clients['prvs_npf'].isnull(), 0),
                                     ]))
 
-        # Исключаем клиентов, которые подписали договор после законного возраста выхода на пенсию
-        #!!!!
+        clients['accnt_bgn_date'] = pd.to_datetime(clients['accnt_bgn_date'])
+        # Возраст на момент подписания договора
+        clients['age_agreement_start'] = clients['accnt_bgn_date'].dt.year - clients['brth_yr']
+
+        # Для трейна переносим клиента в прошлое: спустя год после подписания договора
+        # Преобразуем данные для обучения
         if mode == 'train':
+            clients['age'] = (clients['accnt_bgn_date'] + pd.to_timedelta(365*5, 'days')).dt.year - clients['brth_yr']
+            # Дата начала выплат = дата окончания договора = дата выхода на пенсию
+            clients_on_pension_df = clients[clients['accnt_status'] == 'Выплатной период'].copy()
+            clients_on_pension_df['accnt_end_date'] = clients_on_pension_df['accnt_bgn_date'] + pd.to_timedelta(clients_on_pension_df['cprtn_prd_d'], unit = 'days')
+
+            clients_not_on_pension_df = clients[clients['accnt_status'] == 'Накопительный период'].copy()
+            clients_not_on_pension_df['accnt_end_date'] = clients_not_on_pension_df['accnt_bgn_date'] + (clients_not_on_pension_df['pnsn_age'] - clients_not_on_pension_df['age_agreement_start']).apply(lambda x: pd.to_timedelta(int(x)*365, unit = 'days'))
+
+            #pd.to_datetime('2030-01-01') #заглушка
+            clients = clients.merge(pd.concat([clients_on_pension_df, clients_not_on_pension_df]).loc[:, ['clnt_id', 'accnt_end_date']], on='clnt_id', how='left')
+            
+            clients['accnt_end_age'] = clients['accnt_end_date'].dt.year - clients['brth_yr']
+            # Если клиента перекинули через дату окончания договора
+            # то убираем его из выборки для обучения
+            clients['age_over_accnt_end_flg'] = clients['accnt_end_age'].case_when(([
+                (clients['accnt_end_age'] <= clients['age'], 1),
+                (clients['accnt_end_age'] > clients['age'], 0)
+            ]))
+            clients = clients[clients['age_over_accnt_end_flg'] == 1]
+
+
+            # Исключаем клиентов, которые подписали договор после законного возраста выхода на пенсию
             clients.drop(index = clients[clients['age_agreement_start'] > clients['pnsn_age']].index, inplace=True)
 
-        # Дата начала выплат
-        clients_on_pension = clients[clients['accnt_status'] == 'Выплатной период'].copy()
-        clients_on_pension['accnt_end_date'] = clients_on_pension['accnt_bgn_date'] + clients_on_pension['cprtn_prd_d'].apply(lambda x: pd.to_timedelta(x, unit = 'days'))
+        # Для инференса на тестовых данных Верим что вы придержитесь логики своего бизнес процесса и подадите возраст < pnsn_age
+        # и можем без утечки данных использовать колонку 
+        elif mode == 'inference':
+            clients['age'] = clients['prsnt_age']
 
-        clients_not_on_pension = clients[clients['accnt_status'] == 'Накопительный период'].copy()
-        clients_not_on_pension['accnt_end_date'] = pd.to_datetime('2030-01-01')
-        clients = clients.merge(pd.concat([clients_on_pension, clients_not_on_pension]).loc[:, ['clnt_id', 'accnt_end_date']], on='clnt_id', how='inner')
-
-
-        # Добавляем дату выхода на пенсию по закону
-        clients['accnt_expected_end_date'] = clients['accnt_bgn_date'] + (clients['pnsn_age'] - clients['age_agreement_start']).apply(lambda x: pd.to_timedelta(int(x)*365, unit = 'days'))
-
-        # Добавляем дату отсчета (клиент в прошлом)
-        clients['accnt_past_date'] = clients['erly_pnsn_flg'].case_when(([
-            (clients['erly_pnsn_flg'] == 0,  #when
-                clients['accnt_bgn_date'] + ((clients['accnt_expected_end_date'] - clients['accnt_bgn_date']).dt.days)\
-                                        .apply(lambda x: pd.to_timedelta(int(x * random.uniform(0.2, 0.7)), unit='days'))   #then
-            ),
-            (clients['erly_pnsn_flg'] == 1,  #when
-                clients['accnt_bgn_date'] + ((clients['accnt_end_date'] - clients['accnt_bgn_date']).dt.days)\
-                                        .apply(lambda x: pd.to_timedelta(int(x * random.uniform(0.2, 0.7)), unit='days'))   #then
-            )
-        ]))
-        clients['accnt_past_date'] = pd.to_datetime(clients['accnt_past_date'])
-        clients['accnt_past_age'] = clients['accnt_past_date'].dt.year - clients['brth_yr']
-        
-        clients['accnt_past_date'] = clients['accnt_past_date'].astype(np.int64) // 1e9
-        clients['accnt_bgn_date'] = clients['accnt_bgn_date'].astype(np.int64) // 1e9
 
         clients_columns_typing = self.config['data']['columns']['clients']
         clients_columns = list(self.config['data']['columns']['clients'].keys())
@@ -147,3 +145,5 @@ class DataProcessorV2:
 
         return self.processed_data
 
+df = DataProcessorV2('backend/models/configs/', 'processor_v2.yaml').process(transactions=None, clients=pd.read_csv('sample_cntrbtrs_clnts_ops_trn-3.csv', sep = ';', encoding = 'cp1251'), mode='train')
+print(df)
